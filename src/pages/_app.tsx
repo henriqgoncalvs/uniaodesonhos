8366db3e8630d9bf4b3d2635e9f4e9f1b8c9da3d
Henrique Gonçalves <henriquesg09@hotmail.com>
2021-06-15 13:06:15 -0300

    Main: add 'Ajude a União de Sonhos' toast

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,10 @@
+import { useEffect } from 'react';
+import { toast } from 'react-toastify';
 import { AppProps } from 'next/app';
 import Router from 'next/router';
 import NProgress from 'nprogress';
 
+import { AnchorButton } from 'lib/components/common/Button';
 import GlobalProvider from 'lib/contexts/GlobalProvider';
 
 Router.events.on('routeChangeStart', () => NProgress.start());
@@ -9,6 +12,14 @@ Router.events.on('routeChangeComplete', () => NProgress.done());
 Router.events.on('routeChangeError', () => NProgress.done());
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    toast.warning(
+      <AnchorButton colorStyle="blueFilled">
+        Ajude a União de Sonhos
+      </AnchorButton>,
+    );
+  }, []);
+
   return (
     <GlobalProvider>
       <div id="modal-root" />
